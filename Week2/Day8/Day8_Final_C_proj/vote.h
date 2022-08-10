45dfc3ca4dbf1376b11b616dd33d9b9455e2f6df
Ahmed-Aseel <95757948+Ahmed-Aseel@users.noreply.github.com>
2022-08-10 19:56:41 +0200

    Add files via upload

---
+++ Week2/Day8/Day8_Final_C_proj/vote.h
@@ -0,0 +1,13 @@
+// Header protection
+#ifndef VOTE_H_
+#define VOTE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Functions
+void Cast_Vote();
+void Find_VoteCount();
+void Find_Leading();
+
+#endif
